95ea451bd31fa003eb052a34babb29acf64375ae
brobertson3 <brobertson3@users.noreply.github.com>
2020-03-26 21:02:04 -0700

    Added API call to YouTube using axios

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from "axios";
+
 const KEY = "AIzaSyB9ZBnW0UIqrE5JOsXocb_pZQFaKM8qhms";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    type: "video",
+    maxResults: 5,
+    key: KEY
+  }
+});
